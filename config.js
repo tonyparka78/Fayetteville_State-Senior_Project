83b16cb32ae80c6cc4446af75b19df57a2d0f9b9
Anthony Parker <tonyparka7838@gmail.com>
2019-11-10 09:31:05 -0500

    config.js edit

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 var config = {
     database: {
-        host: 'localhost',     // Database host
-        user: 'project',         // Database username
-        password: 'Admin2019!!',  // Database password
-        port: 3306,            // Default MySQL port
-        db: 'app'              // Database name
     },
     server: {
         host: 'localhost',
@@ -16,7 +11,4 @@ var config = {
 
 /* The module.exports or exports is a special object which is included in
 every JS file in the Node.js application by default. module is a variable
-that represents current module and exports is an object that will be exposed
-as a module. So, whatever you assign to module.exports or exports, will be
-exposed as a module.*/
 module.exports = config
